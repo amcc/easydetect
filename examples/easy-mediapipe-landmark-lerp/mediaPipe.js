d02ff103368db0b0a7a2d85424da2e4dde1dc520
amcc <alistairmcclymont@gmail.com>
2024-01-24 22:11:27 +0000

    model changes

--- examples/easy-mediapipe-landmark-lerp/mediaPipe.js
+++ examples/easy-mediapipe-landmark-lerp/mediaPipe.js
@@ -23,7 +23,10 @@ const createPoseLandmarker = async () => {
   );
   poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
     baseOptions: {
-      modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task`,
+      // modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task`,
+      // modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/latest/pose_landmarker_lite.task`,
+      modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_full/float16/latest/pose_landmarker_full.task`,
+      // modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_heavy/float16/latest/pose_landmarker_heavy.task`,
       delegate: "GPU",
     },
     runningMode: runningMode,
